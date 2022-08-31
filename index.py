fb53b42b945e7d8ac41cbec8298572f98b31293a
Caio Cavalcante <danielcaio75@gmail.com>
2022-08-31 00:30:22 -0300

    Comentarios

--- index.py
+++ index.py
@@ -1,7 +1,4 @@
 from array import *
-# from locale import resetlocale
-# from tkinter.messagebox import RETRY
-
 
 alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V',
@@ -10,11 +7,13 @@ alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 letterShift = 2
 
 def convert(b, e, n):
+    #faz a conversao do texto
     return pow(b, e) % n
 
 def criptografar(texto, shift):
     resultado = []
     for a in texto:
+        #converte cada letra
         a = convert(alfabeto.index(a) + shift, e, n)
         resultado.append(a)
     saveMessage(resultado)
@@ -23,6 +22,7 @@ def criptografar(texto, shift):
 def desencriptar(array):
     resultado = ""
     for a in array:
+        #converte cada letra
         a = convert(a, d, n)
         resultado += alfabeto[a - letterShift]
     print("d: %d" % d)
@@ -77,11 +77,14 @@ def saveLock(p, q, l, n):
     f.close()
 
 def modinv(a, m):
+    #testa um por um os valores de 1 a m
     for x in range(1, m):
+        #checa se x é inverso de a mod m
         if (a * x) % m == 1:
             return x
     return None
 
+#prompt para o usuario escolher a programação
 programa = int(input("Digite\n1-Gerar chave publica\n2-Encriptar\n3-Desencriptar\n4-Gerar Decriptador\n"))
 
 if programa == 1:
@@ -95,7 +98,7 @@ if programa == 1:
 
 elif programa == 2:
     # Encriptar
-    chaves = getLock()
+    chaves = getLock() #puxa 
     p, q, e, n = chaves
     texto = input("digite seu texto:\n").upper()
     texto = criptografar(texto, letterShift)
@@ -105,9 +108,11 @@ elif programa == 2:
 elif programa == 3:
     # Desencriptar
     if(int(input("Carregar chaves ou digitar?\n0-Digitar\n1-Carregar\n"))):
+        #carregar
         p, q, d = getKey()
         n = p * q
     else:
+        #digitar
         p = int(input('digite o p:'))
         q = int(input('digite o q:'))
         d = int(input('digite o d:'))
@@ -118,11 +123,14 @@ elif programa == 3:
 elif programa == 4:
     # Gerar d
     if(int(input("Carregar chaves ou digitar?\n0-Digitar\n1-Carregar\n"))):
+        #carregar
         p, q, e, n = getLock()
     else:
+        #digitar
         p = int(input('digite o p:'))
         q = int(input('digite o q:'))
         e = int(input('digite o e:'))
+    #calcula o d
     d = modinv(e, (p - 1) * (q - 1))
     saveKey(p, q, d)
     print(d)
