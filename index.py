2ca0f633caf0a927410d69082f0df9d803cb5503
Caio Cavalcante <danielcaio75@gmail.com>
2022-08-30 23:56:40 -0300

    commit RSA encryption

--- index.py
+++ index.py
@@ -9,11 +9,14 @@ alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 
 letterShift = 2
 
-def criptografar(texto):
+def convert(b, e, n):
+    return pow(b, e) % n
+
+def criptografar(texto, shift):
     resultado = []
     for i in range(len(texto)):
-        b = alfabeto.index(texto[i]) + letterShift
-        a = (b ** e) % n
+        b = alfabeto.index(texto[i]) + shift
+        a = convert(b, e, n)
         resultado.append(a)
     f = open("message.txt", "w+")
     f.write("%s" % resultado)
@@ -23,8 +26,9 @@ def criptografar(texto):
 def desencriptar(array):
     resultado = ""
     for a in array:
-        a = (a ** d) % n
+        a = convert(a, d, n)
         resultado += alfabeto[a - letterShift]
+    print("d: %d" % d)
     return resultado
 
 def getMessage():
@@ -43,63 +47,51 @@ def saveMessage(text):
     f.close()
 
 def getKeys():
-    f = open("key.txt", "r")
+    f = open("lock.txt", "r")
     content = f.read()
     content = content.split(',')
     for i in range(len(content)):
         content[i] = int(content[i])
     f.close()
-    # zip([p, q, e, n], content)
     return content
 
-def saveKeys(p, q, l, n):
-    resultado = str(p) + ',' + str(q) + ',' + str(l) + ',' + str(n)
+def saveKey(p, q, d):
+    resultado = str(p) + ',' + str(q) + ',' + str(d)
     f = open("key.txt", "w+")
     f.write("%s" % resultado)
     print("salvo :)")
     f.close()
 
-def keysCompatible(p, q, l, n):
-    if((l % 2) == 0):
-        print("Este numero nao pode ser par")
-        return False
-    return True
+def saveLock(p, q, l, n):
+    resultado = str(p) + ',' + str(q) + ',' + str(l) + ',' + str(n)
+    f = open("lock.txt", "w+")
+    f.write("%s" % resultado)
+    print("salvo :)")
+    f.close()
 
-# def egcd(a, b):
-#     if a == 0:
-#         return (b, 0, 1)
-#     else:
-#         g, y, x = egcd(b % a, a)
-#         return (g, x - (b // a) * y, y)
+def modinv(a, m):
+    for x in range(1, m):
+        if (a * x) % m == 1:
+            return x
+    return None
 
-# def modinv(a, m):
-#     g, x, y = egcd(a, m)
-#     if g != 1:
-#         raise Exception('modular inverse does not exist')
-#     else:
-#         return x % m
-
-programa = int(input("Digite \n1-Gerar chave publica\n2-Encriptar\n3-Desencriptar\n"))
+programa = int(input("Digite\n1-Gerar chave publica\n2-Encriptar\n3-Desencriptar\n4-Gerar Decriptador\n"))
 
 if programa == 1:
     # Gerar chave publica
     p = int(input("digite um numero primo:\n"))
     q = int(input("digite outro numero primo:\n"))
-    l = int(input("digite um numero relativamente primo aos numeros sucessores dos anteriores:\nsugestão: 3\n"))
-    n = p * q
-    if(keysCompatible(p, q, l, n)):
-        saveKeys(p, q, l, n)
-        print("chave: %d" % n)
-    else:
-        print("chaves nao compativeis")
-
+    e = int(input("digite um numero relativamente primo aos numeros sucessores dos anteriores\n"))
+    n = p * q 
+    saveLock(p, q, e, n)
+    print("chave: %d" % n)
 
 elif programa == 2:
     # Encriptar
     chaves = getKeys()
-    p, q, e, n = chaves #isso funciona igual a
+    p, q, e, n = chaves
     texto = input("digite seu texto:\n").upper()
-    texto = criptografar(texto)
+    texto = criptografar(texto, letterShift)
     print(texto)
     
 
@@ -107,13 +99,24 @@ elif programa == 3:
     # Desencriptar
     if(int(input("Carregar chaves ou digitar?\n0-Digitar\n1-Carregar\n"))):
         chaves = getKeys()
+        p, q, d, n = chaves
+    else:
+        p = int(input('digite o p:'))
+        q = int(input('digite o q:'))
+        d = int(input('digite o d:'))
+        n = p * q
+    message = getMessage()
+    print(desencriptar(message))
+
+elif programa == 4:
+    # Gerar d
+    if(int(input("Carregar chaves ou digitar?\n0-Digitar\n1-Carregar\n"))):
+        chaves = getKeys()
         p, q, e, n = chaves
     else:
         p = int(input('digite o p:'))
         q = int(input('digite o q:'))
         e = int(input('digite o e:'))
-        n = p * q
-    # d = modinv(e, (p - 1) * (q - 1))
-    d = pow(e, -1, (p - 1) * (q - 1))
-    message = getMessage()
-    print(desencriptar(message))
+    d = modinv(e, (p - 1) * (q - 1))
+    saveKey(p, q, d)
+    print(d)
