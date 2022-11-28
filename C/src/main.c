ff0b6f016dd8703485191a7485b862175f592f7d
Caio Cavalcante <danielcaio75@gmail.com>
2022-11-28 08:47:53 -0300

    add C code

--- C/src/main.c
+++ C/src/main.c
@@ -1,96 +1,23 @@
-/*
-
-    RSA Cryptography Library
-    by:  Caio Cavalcante
-
-*/
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <time.h>
+#include <gmp.h>
 #include "rsa.h"
-
-#define __str_len__ 1000
-#define __letter_shift__ 2
 
 int main()
 {
-    int state;
-    //state selector
-    printf("RSA Cryptography Library\nChoose an option:\n1 - Encrypt\n2 - Decrypt\n3 - Generate keys\n");
-    scanf("%d", &state);
-
-    switch (state)
+    int opcao;
+    printf("1 - Criptografar\n2 - Descriptografar\n3 - Gerar chaves\n");
+    scanf("%d", &opcao);
+    switch(opcao)
     {
         case 1:
-        {
-            //encrypt
-            char message[__str_len__];
-            int e, n, len;
-    
-            printf("Enter the message to encrypt: ");
-            scanf(" %[^\n]", message);
-
-            len = strlen(message);
-
-            for(int i = 0; i < len; i++)
-                for(int j = 0; j < 28; j++)
-                    if(message[i] == alphabet[j])
-                    {
-                        message[i] = j + __letter_shift__;
-                        break;
-                    }
-
-            for(int i = 0; i < len; i++)
-                printf("%d ", message[i]);
-
-            printf("\n");
-            printf("Enter the public key (e, n): ");
-            scanf("%d %d", &e, &n);
-            encrypt(message, e, n);
+            criptografar();
             break;
-        }
         case 2:
-        {
-            //decrypt
-            int d, n, message[__str_len__];
-
-            printf("Enter the code to decrypt: ");
-            for(int i = 0;i < __str_len__;i++)
-                if(scanf("%d", &message[i]) == EOF || message[i] == -1)
-                {
-                    message[i] = -1;
-                    break;
-                }
-
-            printf("Enter the private key (d, n): ");
-            scanf("%d %d", &d, &n);
-            decrypt(message, d, n, __letter_shift__);
+            descriptografar();
             break;
-        }
         case 3:
-        {
-            //generate keys
-            int p, q;
-            printf("Enter two prime numbers: ");
-            scanf("%d %d", &p, &q);
-            create_key(p, q);
-            break;
-        }
-        default:
-        {
-            printf("Invalid option\n");
-            break;
-        }
-    }
-    char *s;
-
-    for(int i = 0; 1; i++)
-    {
-        if(scanf("%s", s) == EOF)
+            gerar_chaves();
             break;
     }
-    return 0;
 }
--- C/src/rsa.h
+++ C/src/rsa.h
@@ -1,133 +1,157 @@
-#define k 2
+#define fast_pow_mod(a, b, c, d) mpz_powm(a, b, c, d)
+#define LETTER_SHIFT 2
 
-typedef unsigned long long int ulli;
-
-char alphabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-
-ulli fast_pow(int base, int exp)
+void criptografar()
 {
-    ulli result = 1;
-    for (;;)
-    {
-        if (exp & 1)
-            result *= base;
-        exp >>= 1;
-        if (!exp)
-            break;
-        base *= base;
-    }
+    mpz_t n, e, aux; 
+    mpz_init(n); mpz_init(e); mpz_init(aux);
 
-    return result;
-}
-int pow_mod(int base, int exp, int mod)
-{
-    int result = 1;
-    for (;;)
-    {
-        if (exp & 1)
-            result = (result * base) % mod;
-        exp >>= 1;
-        if (!exp)
-            break;
-        base = (base * base) % mod;
-    }
+    FILE* ptr = fopen("lock", "r");
+    char s[1000], t[1000];
 
-    return result;
-}
-int mod_inv(int a, int m)
-{
-    for (int i = 1; i < m; i++)
-        if ((a * i) % m == 1)
-            return i;
+    fscanf(ptr, "%s", s);
 
-    return -1;
-}
-int mdc(int a, int b)
-{
-    if (a == 0)
-        return b;
-    return mdc(b % a, a);
-}
-int is_relative_prime(int a, int b)
-{
-    return mdc(a, b) == 1;
-}
-int is_prime(int n)
-{
-    if (n <= 1)
-        return 0;
-    if (n <= 3)
-        return 1;
+    mpz_set_str(n, s, 10);
 
-    if (n % 2 == 0 || n % 3 == 0)
-        return 0;
+    fscanf(ptr, "%s", s);
 
-    for (int i = 5; i * i <= n; i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
+    mpz_set_str(e, s, 10);
 
-    return 1;
-}
-/*
-    params:
-        p: prime number
-        q: prime number
-*/
-int create_key(int p, int q)
-{
-    if (!is_prime(p) || !is_prime(q))
-    {
-        printf("p and q must be prime numbers");
-        return 0;
-    }
+    ptr = fopen("msg", "r");
+
+    fgets(s, 1000, ptr);
+
+    fclose(ptr);
+
+    ptr = fopen("cypher", "w");
     
-    int n = p * q;
-
-    int phi = (p - 1) * (q - 1);
-
-    int e = 3;
-
-    while (e < phi)
+    for(int i = 0; i < strlen(s); i++)
     {
-        if (is_relative_prime(e, phi))
-            break;
-        e++;
-    }
-
-    int d = (1 + (k * phi)) / e;
-
-    printf("Public key: (%d, %d)\n", e, n);
-
-    printf("Private key: (%d, %d)\n", d, n);
-
-    return 0;
-}
-void encrypt(char message[], int e, ulli n)
-{
-    //e = expoente
-    //n = chave de codificação
-    printf("Encrypted message:\n");
-    int i = 0;
-    ulli c;
-    while (message[i] != '\0')
-    {
-        printf("%llu ", fast_pow(message[i], e) % n);
-        i++;
+        if(s[i] > 'Z' || s[i] < 'A')
+            s[i] = 'Z' + 1;
+        s[i] += LETTER_SHIFT - 'A';
+        mpz_set_ui(aux, s[i]);
+        fast_pow_mod(aux, aux, e, n);
+        gmp_printf("%Zd ", aux);
+        mpz_get_str(t, 10, aux);
+        fprintf(ptr, "%s ", t);
     }
     printf("\n");
+    fclose(ptr);
 }
-void decrypt(int message[], int d, int n, int letter_shift)
+
+void descriptografar()
 {
-    //d = expoente
-    //n = chave de codificação
-    int i = 0;
     char c;
-    printf("Decrypted message:\n");
-    while (message[i] != -1)
-    {
-        printf("%c", (char)alphabet[pow_mod(message[i], d, n) - letter_shift]);
+    mpz_t n, d, aux;
 
-        i++;
+    mpz_init(n);mpz_init(d);mpz_init(aux);
+
+    FILE* ptr = fopen("key", "r");
+
+    char s[1000];
+
+    fscanf(ptr, "%s", s);
+    printf("%s\n", s);
+
+    mpz_set_str(n, s, 10);
+
+    fscanf(ptr, "%s", s);
+
+    mpz_set_str(d, s, 10);
+
+    fclose(ptr);
+
+    ptr = fopen("cypher", "r");
+
+    while(1)
+    {
+        if(fscanf(ptr, "%s", s) == EOF)
+            break;
+
+        mpz_set_str(aux, s, 10);
+
+        fast_pow_mod(aux, aux, d, n);
+        c = mpz_get_ui(aux) - LETTER_SHIFT + 'A';
+        if(c > 'Z' || c < 'A')
+            c = ' ';
+        printf("%c", c);
     }
     printf("\n");
+    fclose(ptr);
+}
+
+void gerar_chaves()
+{
+
+    int opcao;
+    mpz_t p, q, n, phi, e, d, aux0, aux1;
+    mpz_init(p); mpz_init(q); mpz_init(n); mpz_init(phi); mpz_init(e); mpz_init(d); mpz_init(aux0), mpz_init(aux1);
+
+    FILE* ptr;
+    ptr = fopen("chaves", "r");
+    if(ptr == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        return;
+    }
+
+    char s[1000];
+
+    fscanf(ptr, "%s", s);
+
+    mpz_set_str(p, s, 10);
+    gmp_printf("p: %Zd\n", p);
+
+    fscanf(ptr, "%s", s);
+
+    mpz_set_str(q, s, 10);
+    gmp_printf("q: %Zd\n", q);
+
+    mpz_mul(n, p, q);
+
+    mpz_sub_ui(aux0, p, 1);
+    mpz_sub_ui(aux1, q, 1);
+
+    mpz_mul(phi, aux0, aux1);
+
+    if(fscanf(ptr, "%s", s) != EOF)
+        mpz_set_str(e, s, 10);
+    else
+    {
+        printf("gerando e\n");
+        mpz_set_ui(e, 3);
+        while(1)
+        {
+            mpz_gcd(aux0, e, phi);
+            if(mpz_cmp_ui(aux0, 1) == 0)
+                break;
+            mpz_add_ui(e, e, 2);
+        }
+    }
+    fclose(ptr);
+
+    mpz_invert(d, e, phi);
+    
+    ptr = fopen("chaves", "w");
+
+    fprintf(ptr, "%s\n", mpz_get_str(s, 10, p));
+    fprintf(ptr, "%s\n", mpz_get_str(s, 10, q));
+    fprintf(ptr, "%s\n", mpz_get_str(s, 10, e));
+    fprintf(ptr, "%s\n", mpz_get_str(s, 10, d));
+    fprintf(ptr, "%s\n", mpz_get_str(s, 10, n));
+    fclose(ptr);
+
+    ptr = fopen("lock", "w");
+    fprintf(ptr, "%s\n", mpz_get_str(s, 10, n));
+    fprintf(ptr, "%s\n", mpz_get_str(s, 10, e));
+    fclose(ptr);
+
+    ptr = fopen("key", "w");
+    fprintf(ptr, "%s\n", mpz_get_str(s, 10, n));
+    fprintf(ptr, "%s\n", mpz_get_str(s, 10, d));
+    fclose(ptr);
+
+    gmp_printf("chave publica: (%Zd, %Zd)\n", e, n);
+    gmp_printf("chave privada: (%Zd, %Zd)\n", d, n);
 }
